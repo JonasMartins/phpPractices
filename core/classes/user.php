b04b99bd3b7b8baef4ff4cd99c221164d3ed3a1e
JonasMartins <jonas.w.martins@gmail.com>
2017-12-30 18:22:13 -0300

    010

--- core/classes/user.php
+++ core/classes/user.php
@@ -38,6 +38,25 @@ class User
 
 	}
 
+	public function userData($user_id)
+	{
+		$stmt = $this->pdo->prepare("
+			SELECT * 
+			FROM `users`
+			WHERE `user_id` = :user_id
+			");
+		$stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_OBJ);
+	}
+
+	public function logout()
+	{
+		$_SESSION = array();
+		session_destroy();
+		header('Location: ../index.php');
+	}
+
 } 
 
 ?>
