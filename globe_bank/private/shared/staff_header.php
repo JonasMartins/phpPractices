b657990d55bd058a502f2e6f7d7d2a93a88ea3eb
JonasMartins <jonas.w.martins@gmail.com>
2018-01-26 21:10:53 -0300

    et1 1.6

---
+++ globe_bank/private/shared/staff_header.php
@@ -0,0 +1,17 @@
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" type="text/css" href="../stylesheet/staff.css">
+  </head>
+
+  <body>
+  <header>
+  	<h1>GBI Staff Area</h1>
+  </header>
+
+  <navigation>
+  	<ul>
+  		<li><a href="index.php">Menu</a></li>
+  	</ul>
+  </navigation>
