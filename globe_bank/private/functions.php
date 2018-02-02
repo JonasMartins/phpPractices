d615cf86aab86b99bc945381d4800f73d619070f
JonasMartins <jonas.w.martins@gmail.com>
2018-02-02 20:49:52 -0300

    et1 15

--- globe_bank/private/functions.php
+++ globe_bank/private/functions.php
@@ -19,7 +19,21 @@ function raw_u($url="")
 	return rawurlencode($url);
 }
 
-function h($string="") {
+function h($string="") 
+{
   return htmlspecialchars($string);
 }
+
+function error_404() 
+{
+  header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+  exit();
+}
+
+function error_500() 
+{
+  header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+  exit();
+}
+
 ?>
