076945ca96a70d31839b440a96a9e7674e82c9e7
JonasMartins <jonas.w.martins@gmail.com>
2018-02-07 18:29:51 -0300

    et1 23

--- globe_bank/public/staff/pages/index.php
+++ globe_bank/public/staff/pages/index.php
@@ -17,32 +17,32 @@
     <h1>Pages</h1>
 
     <div class="actions">
-      <a class="action" href="">Create New Page</a>
+      <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
     </div>
 
-  	<table class="list">
-  	  <tr>
+    <table class="list">
+      <tr>
         <th>ID</th>
         <th>Position</th>
         <th>Visible</th>
-  	    <th>Name</th>
-  	    <th>&nbsp;</th>
-  	    <th>&nbsp;</th>
+        <th>Name</th>
         <th>&nbsp;</th>
-  	  </tr>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+      </tr>
 
       <?php foreach($pages as $page) { ?>
         <tr>
           <td><?php echo h($page['id']); ?></td>
           <td><?php echo h($page['position']); ?></td>
           <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
-    	    <td><?php echo h($page['menu_name']); ?></td>
+          <td><?php echo h($page['menu_name']); ?></td>
           <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
-          <td><a class="action" href="">Edit</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
           <td><a class="action" href="">Delete</a></td>
-    	  </tr>
+        </tr>
       <?php } ?>
-  	</table>
+    </table>
 
   </div>
 
