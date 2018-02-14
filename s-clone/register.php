f9e7210397260a201c061306f624c454eff53bdc
JonasMartins <jonas.w.martins@gmail.com>
2018-02-14 14:38:08 -0300

    sc 003

--- s-clone/register.php
+++ s-clone/register.php
@@ -17,6 +17,48 @@
 			</p>
 			<button type="submit" name="loginButton">Login</button>
 		</form>
+
+		<form id="resgisterForm" action="register.php" method="POST">
+			<h2>Create an Account</h2>
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="username" required />
+			</p>
+
+			<p>
+				<label for="firstName">First Name</label>
+				<input id="firstName" name="firstName" type="text" required />
+			</p>
+
+			<p>
+				<label for="lastName">Last Name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="username" required />
+			</p>
+
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="user@email.com" required />
+			</p>
+
+			<p>
+				<label for="confirmEmail">Confirm Email</label>
+				<input id="confirmEmail" name="confirmEmail" type="email" required />
+			</p>
+
+
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" required />				
+			</p>
+
+			<p>
+				<label for="confirmPassword">Confirm Password</label>
+				<input id="confirmPassword" name="confirmPassword" type="password" required />				
+			</p>
+
+			<button type="submit" name="registerButton">Sign Up</button>
+		</form>
+
 	</div>
 </body>
 </html>
