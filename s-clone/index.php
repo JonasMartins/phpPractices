4393d6dd04e5003f95590259b0ffc314c6a1e455
JonasMartins <jonas.w.martins@gmail.com>
2018-02-25 20:38:53 -0300

    sc 039

--- s-clone/index.php
+++ s-clone/index.php
@@ -1,21 +1,24 @@
 <?php 
 	include("includes/config.php");
 
-	session_destroy();
+	// session_destroy();
 
-	if(isset($_SESSION['userLoggedIn']))
-	{
-		$userLoggedIn = $_SESSION['userLoggedIn'];
-	} else 
-		header("Location: register.php");
+	// if(isset($_SESSION['userLoggedIn']))
+	// {
+	// 	$userLoggedIn = $_SESSION['userLoggedIn'];
+	// } else 
+	// 	header("Location: register.php");
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Slotify</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	
+	<div id="nowPlayingBarContainer">
+		
+	</div>	
 </body>
 </html>
