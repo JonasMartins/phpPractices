6ad01a6d4fca699c8c4d98948aa1247cd535fff7
JonasMartins <jonas.w.martins@gmail.com>
2018-03-13 20:03:20 -0300

    crf 008

--- crfc/react_basics/src/index.js
+++ crfc/react_basics/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
-	return <h1>Hello World</h1>
+	// return React.createElement('h1',{className:'example'},'Hello World')
+	//funciona contanto que esteja tudo dentro de apenas uma tag
+	
+	return (
+		<div className='example'>
+			<h1>Hello World</h1>
+			<p>Test</p>
+		</div>
+		)
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
