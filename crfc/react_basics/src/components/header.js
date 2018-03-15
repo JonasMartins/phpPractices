76bf38342a39615dff6df9706fd864016f4b12b2
JonasMartins <jonas.w.martins@gmail.com>
2018-03-15 20:52:44 -0300

    crf 009

---
+++ crfc/react_basics/src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+	return <div><h1>Header</h1></div>
+}
+
+// the name of component to share with another components
+export default Header;
--- crfc/react_basics/src/index.js
+++ crfc/react_basics/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
+
+import Header from './components/header';
+
+
 const App = () =>{
 	// return React.createElement('h1',{className:'example'},'Hello World')
 	//funciona contanto que esteja tudo dentro de apenas uma tag
 	
 	return (
-		<div className='example'>
-			<h1>Hello World</h1>
-			<p>Test</p>
+		<div>
+			<Header/>
 		</div>
+
 		)
 }
-
 ReactDOM.render(<App/>,document.querySelector('#root'));
