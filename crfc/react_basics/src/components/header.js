6e0efcca9256c59ad83e2533bd966796720d10b4
JonasMartins <jonas.w.martins@gmail.com>
2018-03-16 21:26:30 -0300

    crf 010

--- crfc/react_basics/src/components/header.js
+++ crfc/react_basics/src/components/header.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+const user = {
+	name:'Francis',
+	lastname:'Jones',
+	age:16
+}
+
+const getYear = () => {
+	const newDate = new Date();
+	return newDate.getFullYear();	
+}
+
 const Header = () => {
-	return <div><h1>Header</h1></div>
+	// return <div>
+	// <h1>The date is</h1>{
+	// 	getYear()	
+	// }
+	// </div>
+
+	return (
+		<div>
+			<div>{user.name}</div>
+			<div>{user.lastname}</div>
+			<div>{user.age}</div>
+			
+		</div>
+		)
+
+
 }
 
 // the name of component to share with another components
