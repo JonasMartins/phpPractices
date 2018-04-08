070827bbd4e3c065f12dd972cd4d44eae32813f6
JonasMartins <jonas.w.martins@gmail.com>
2018-04-07 22:46:26 -0300

    crf 026

--- crfc/router/src/index.js
+++ crfc/router/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter,Route,Link } from 'react-router-dom';
+import { HashRouter,Route,Link } from 'react-router-dom';
 
-
+// MemoryRouter esconde toda a url, fica sempre a mesma do home
 // compoents
 import Home from './components/home';
 import Posts from './components/posts';
 import Profile from './components/profile';
+import PostItem from './components/post_item';
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<div>
 				<header>
 					<Link to="/">Home</Link><br/>
@@ -21,10 +22,11 @@ const App = () => {
 					<hr/>
 				</header>
 				<Route path="/" exact component={Home}/>
-				<Route path="/posts" component={Posts}/>
+				<Route path="/posts" exact component={Posts}/>
+				<Route path="/posts/:id" component={PostsItem}/>
 				<Route path="/profiles" component={Profile}/>
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 
 		)
 }
