f1948f389bae67a7fea9a30f1955a0e08b52bed1
JonasMartins <jonas.w.martins@gmail.com>
2018-04-14 20:29:00 -0300

    crf 030

--- crfc/router/src/components/lifecycles.js
+++ crfc/router/src/components/lifecycles.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class Life extends Component {
+	state = {
+		title: "Life Cycle"
+	}
+
 	// 1 get default props
 	
 	// 2 get default state 
@@ -11,12 +15,44 @@ class Life extends Component {
 	}
 	// 4 redner jsx
 	
+	componentWillUpdate(){
+		console.log('Before Update')
+	}
+
+	componentDidUpdate(){
+		console.log('After Update')
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		// check previous and next state and choose if it should or shouldn't update
+		// if return false will not update, true otherwise 
+		console.log(this.state.title);
+		console.log(nextState.title);
+		if(nextState.title === 'Something else')
+			return false
+		return true;
+	}
+
+	componentWillReceiveProps(){
+		console.log('Before receive props')
+
+	}
+
+	componentWillUnmount(){
+		console.log('Unmount')
+		
+	}
+
 	render(){
+		console.log('Render')
 		return (
 			<div>
-				<h3>Life Cycles</h3>
+				<h3>{this.state.title}</h3>
+				<div> onClick={ () => this.state({
+					title:'Something else'
+				}) }click to change</div>
 			</div>
-		)
+		) 
 	}
 
 	// 5 after jsx
