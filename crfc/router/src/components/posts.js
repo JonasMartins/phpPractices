2bb345472053fb070788ce055420b093ea7bed95
JonasMartins <jonas.w.martins@gmail.com>
2018-04-17 20:22:14 -0300

    crf 033

--- crfc/router/src/components/posts.js
+++ crfc/router/src/components/posts.js
@@ -3,13 +3,35 @@ import { Link } from 'react-router-dom';
 
 
 const Posts = () => {
-	return (
-		<div>
-			<Link to="/posts/1">Post1</Link><br/>
-			<Link to="/posts/2">Post2</Link><br/>
-			<Link to="/posts/3">Post3</Link>
-		</div>
-		)
+	
+	// const ids = [
+	// 	{id:'1',name:'Post1'},
+	// 	{id:'2',name:'Post2'},
+	// 	{id:'3',name:'Post3'}
+	// ] 
+	// old way 
+	// const list = ids.map( item=> {
+	// 	return (
+	// 			<span key={item.id} >
+	// 				<Link to={item.id}>{item.name}</Link><br />
+	// 			</span>
+	// 		)
+	// })
+
+	// return ids.map( item=> {
+	// 	return (
+	// 			<span key={item.id} >
+	// 				<Link to={item.id}>{item.name}</Link><br />
+	// 			</span>
+	// 		)
+	// })
+
+	return [
+		"Hello",
+		" i am",
+		"a react app"
+	]
+
 }
 
 export default Posts;
